e2625bf01ff3b3584447d370aaa3a1f4f9f4668a
Elias <elias@identione.com>
2024-03-27 13:35:56 +0100

    feat(trafiklab): Controller for fetching lines & stops

---
+++ src/main/java/com/sbab/demo/controllers/ApiController.java
@@ -0,0 +1,43 @@
+package com.sbab.demo.controllers;
+
+import com.sbab.demo.controllers.dto.LineDto;
+import com.sbab.demo.controllers.dto.StopPointDto;
+import com.sbab.demo.services.TrafikLabService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    private final TrafikLabService trafikLabService;
+    private final ModelMapper modelMapper;
+
+    @Autowired
+    public ApiController(TrafikLabService trafikLabService, ModelMapper modelMapper) {
+        this.trafikLabService = trafikLabService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/stops")
+    public List<StopPointDto> getStops() {
+        return trafikLabService.getStops().stream()
+                .filter(stop -> stop.getGid() % 5 == 0)
+                .map(stop -> modelMapper.map(stop, StopPointDto.class))
+                .toList()
+                .subList(0, 10);
+    }
+
+    @GetMapping("/lines")
+    public List<LineDto> getLines() {
+        return trafikLabService.getBusLines().stream()
+                .filter(line -> line.getGid() % 3 == 0)
+                .map(line -> modelMapper.map(line, LineDto.class))
+                .toList()
+                .subList(0, 10);
+    }
+}
